0c5ada647b410b6c586b1e4df582b7f99bbd1411
Angela Edwards <127447078+angelaed96@users.noreply.github.com>
2024-07-31 19:33:58 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from prepare import prepare
+from results import results
+
+def again():
+  again = input("Want to calculate another grade?(Y/N): ")
+  if again == "Y" or again == "y":
+    main() #Return to the begining of the program
+  elif again == "N" or again == "n":
+    print("Good Luck!")
+    quit()
+    
+def main():
+  percent = input("What is your current grade percentage?: ")
+  grade = float(percent)
+  
+  if int(grade) >= 90:
+    passed = True
+    results(passed) #Even if they don't take the final, they would pass
+  else: #89% and under
+    prepare(grade)
+
+main()
+if (again != "N" or again != "n"):
+  again()
+  
